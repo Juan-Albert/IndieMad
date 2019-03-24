d8203b71cc8dec07ae40c41a29d6f8b44f88f4dd
Juan-Albert <juanmartinezlopez96@gmail.com>
2019-03-24 19:43:46 +0100

    Audience

--- EvadGameJam/Assets/Scripts/SpeechManager.cs
+++ EvadGameJam/Assets/Scripts/SpeechManager.cs
@@ -1,8 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SpeechManager : MonoBehaviour
 {
-    
+
+    public Text timer;
+    public GameObject speakerBlue, speakerRed;
+
+
+
+    IEnumerator Timer()
+    {
+        int time = 10;
+
+        while (time > 0)
+        {
+            timer.text = time.ToString();
+            yield return new WaitForSeconds(1f);
+            time--;
+        }
+        GameManager.instance.NextBattle();
+
+    }
 }
