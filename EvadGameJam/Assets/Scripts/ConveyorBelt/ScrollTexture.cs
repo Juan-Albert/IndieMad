2df3345f1aa9891b4dfa664d3804a602073b26bc
Juan-Albert <juanmartinezlopez96@gmail.com>
2019-04-29 23:05:56 +0200

    Conveyor

---
+++ EvadGameJam/Assets/Scripts/ConveyorBelt/ScrollTexture.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+    public float speed = 1f;
+
+    private float currentscroll = 0;
+    private Material _material;
+    void Start()
+    {
+        _material = GetComponent<SpriteRenderer>().material;
+    }
+
+    void Update()
+    {
+        currentscroll += speed * Time.deltaTime;
+        _material.mainTextureOffset = new Vector2(currentscroll, 0);
+    }
+}
